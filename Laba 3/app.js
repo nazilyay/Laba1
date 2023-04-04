08c2c70ff0e0abe3af2a7d30f9765ea42cd513a6
nazilyay <YamaltdinovaNF@stud.kai.ru>
2023-04-04 22:04:45 +0300

    Переключение вкладок

---
+++ Laba 3/app.js
@@ -0,0 +1,26 @@
+var main = function () {
+    "use strict";
+    $(".tabs a:nth-child(1)").on("click", function () {
+        // делаем все вкладки неактивными
+        $(".tabs span").removeClass("active");
+        // делаем активной первую вкладку
+        $(".tabs a:nth-child(1) span").addClass("active");
+        //очищаем основное содержание, чтобы переопределить его
+        $("main .content").empty();
+        //возвращается false, так как мы не переходим по ссылке
+        return false;
+    });
+    $(".tabs a:nth-child(2)").on("click", function () {
+        $(".tabs span").removeClass("active");
+        $(".tabs a:nth-child(2) span").addClass("active");
+        $("main .content").empty();
+        return false;
+    });
+    $(".tabs a:nth-child(3)").on("click", function () {
+        $(".tabs span").removeClass("active");
+        $(".tabs a:nth-child(3) span").addClass("active");
+        $("main .content").empty();
+        return false;
+    });
+};
+main();
