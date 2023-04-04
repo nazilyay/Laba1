5e5248375c30fa6ea132755f519564fdb4148ec4
nazilyay <YamaltdinovaNF@stud.kai.ru>
2023-04-04 22:36:53 +0300

    Итоговая 5

--- Laba 3/app.js
+++ Laba 3/app.js
@@ -1,13 +1,50 @@
 var main = function () {
     "use strict";
+    var toDos = [
+        "Посмотреть напитки",
+        "Посмотреть завтраки",
+        "Посмотреть ланчи",
+        "Посмотреть акции"
+    ];
     $(".tabs a span").toArray().forEach(function (element) {
         // создаем обработчик щелчков для этого элемента
         $(element).on("click", function () {
             $(".tabs a span").removeClass("active");
             $(element).addClass("active");
             $("main .content").empty();
+            var $element = $(element), $content;
+            if ($element.parent().is(":nth-child(1)")) {
+                $content = $("<ul>");
+				for (var i = toDos.length - 1; i > -1; i--) {
+					$content.append($("<li>").text(toDos[i]));
+				}
+				$("main .content").append($content);
+            }
+            else if ($element.parent().is(":nth-child(2)")) {
+                $content = $("<ul>");
+                toDos.forEach(function (todo) {
+                    $content.append($("<li>").text(todo));
+                });
+                $("main .content").append($content);
+            } else if ($element.parent().is(":nth-child(3)")) {
+                $(".content").append("<input>");
+				$(".content").append("<br />");
+				$(".content").append("<button>Добавить</button>");
+				$(".content input").addClass("inputStyle");
+				$(".content br").addClass("clear");
+				$(".content button").addClass("buttonStyle");
+            }
             return false;
         });
     });
+    $(".content").on("click", ".buttonStyle", function() {
+		if ($(".inputStyle").val() != "") {
+			toDos.push($(".inputStyle").val());
+			alert("Предложение успешно добавлено в список!");
+		}
+		else {
+			alert("ERROR: Длина добавляемого предложения должна быть > 0");
+		}
+	});
 };
 main();
