3a8972f1581f1ce8ff84e46a42f97beb4c0e1cb1
nazilyay <YamaltdinovaNF@stud.kai.ru>
2023-04-04 22:07:26 +0300

    Переработка кода с помощью цикла

--- Laba 3/app.js
+++ Laba 3/app.js
@@ -1,23 +1,14 @@
 var main = function () {
     "use strict";
-    var makeTabActive = function (tabNumber) {
-        //сконструируем селектор из tabNubmer
+    var tabNumber;
+    for (tabNumber = 1; tabNumber <= 3; tabNumber++) {
         var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-        $(".tabs span").removeClass("active");
-        $(tabSelector).addClass("active");
-    };
-    $(".tabs a:nth-child(1)").on("click", function () {
-        makeTabActive(1);
-        return false;
-    });
-    $(".tabs a:nth-child(2)").on("click", function () {
-        makeTabActive(2);
-        return false;
-    });
-    $(".tabs a:nth-child(3)").on("click", function () {
-        makeTabActive(3);
-        return false;
-    });
+        $(tabSelector).on("click", function () {
+            $(".tabs span").removeClass("active");
+            $(this).addClass("active");
+            return false;
+        });
+    }
 };
 
 main();
