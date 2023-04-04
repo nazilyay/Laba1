cbebcb1bb33c72fd0e4ca843d2e231d28a0ef7a0
nazilyay <YamaltdinovaNF@stud.kai.ru>
2023-04-04 22:11:40 +0300

    Переработка кода с помощью цикла forEach

--- Laba 3/app.js
+++ Laba 3/app.js
@@ -1,14 +1,13 @@
 var main = function () {
     "use strict";
-    var tabNumber;
-    for (tabNumber = 1; tabNumber <= 3; tabNumber++) {
-        var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-        $(tabSelector).on("click", function () {
-            $(".tabs span").removeClass("active");
-            $(this).addClass("active");
+    $(".tabs a span").toArray().forEach(function (element) {
+        // создаем обработчик щелчков для этого элемента
+        $(element).on("click", function () {
+            $(".tabs a span").removeClass("active");
+            $(element).addClass("active");
+            $("main .content").empty();
             return false;
         });
-    }
+    });
 };
-
 main();
