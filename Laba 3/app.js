27de1b93af53f900dd964bd218e4e062e1ce3532
nazilyay <YamaltdinovaNF@stud.kai.ru>
2023-04-04 22:06:24 +0300

    Преобразование кода с помощью функции

--- Laba 3/app.js
+++ Laba 3/app.js
@@ -1,26 +1,23 @@
 var main = function () {
     "use strict";
-    $(".tabs a:nth-child(1)").on("click", function () {
-        // делаем все вкладки неактивными
+    var makeTabActive = function (tabNumber) {
+        //сконструируем селектор из tabNubmer
+        var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
         $(".tabs span").removeClass("active");
-        // делаем активной первую вкладку
-        $(".tabs a:nth-child(1) span").addClass("active");
-        //очищаем основное содержание, чтобы переопределить его
-        $("main .content").empty();
-        //возвращается false, так как мы не переходим по ссылке
+        $(tabSelector).addClass("active");
+    };
+    $(".tabs a:nth-child(1)").on("click", function () {
+        makeTabActive(1);
         return false;
     });
     $(".tabs a:nth-child(2)").on("click", function () {
-        $(".tabs span").removeClass("active");
-        $(".tabs a:nth-child(2) span").addClass("active");
-        $("main .content").empty();
+        makeTabActive(2);
         return false;
     });
     $(".tabs a:nth-child(3)").on("click", function () {
-        $(".tabs span").removeClass("active");
-        $(".tabs a:nth-child(3) span").addClass("active");
-        $("main .content").empty();
+        makeTabActive(3);
         return false;
     });
 };
+
 main();
